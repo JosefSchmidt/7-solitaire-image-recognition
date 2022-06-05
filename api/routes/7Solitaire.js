42b389a05a37b2ab38c0f6b6d5db245ec65845d4
Steveng85 <s205456@student.dtu.dk>
2022-06-05 11:24:17 +0200

    upload file feature

--- api/routes/7Solitaire.js
+++ api/routes/7Solitaire.js
@@ -1,4 +1,20 @@
+//lib
 const router = require("express").Router();
+const multer = require("multer");
+
+//logic for lib
+const storage = multer.diskStorage({
+  destination: function(req, file, cb){
+    cb(null, "./images");
+  },
+  filename: function(req, file, cb){
+    cb(null, file.fieldname)
+  }
+});
+
+const upload = multer({storage: storage});
+
+router.post("/gameMove", upload.single("takenPicture"), require("../controller/7Solitaire/calculateGameMove"));
 
 router.get("/", require("../controller/7Solitaire/calculateGameMove"))
 
